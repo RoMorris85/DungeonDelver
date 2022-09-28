f256a997861c0e316ae236230364556b2f3fee70
Anthony Morales <amorale8@student.fitchburgstate.edu>
2022-09-27 20:59:29 -0400

    Auto stash before merge of "main" and "origin/main"

---
+++ Dungeon_Project/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject swarmerPrefab;
+    [SerializeField]
+    private float swarmerInterval = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(swarmerInterval, swarmerPrefab));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-4f, 4), Random.Range(-4f, 4f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+}
